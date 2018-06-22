fef06bcd01101558ace3001698ab787da9549b9d
Sarah Li <sarcl@hotmail.com>
2018-06-22 15:23:09 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,32 @@
 # Make your shoe class here!
+# book.rb
+# Add you Book class here
+class Book
+  def initialize(title)
+    @title = title
+  end
+  def title
+    @title
+  end
+  def author = (author)
+    @author = author
+  end
+  def author 
+    @author
+  end
+  def page_count = (num)
+    @page_count = num
+  end
+  def page_count 
+    @page_count
+  end
+  def genre = (genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
