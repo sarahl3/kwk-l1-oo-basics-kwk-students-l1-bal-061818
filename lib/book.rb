6e9101b1ab82e3e7258c1b1d9f9720877dec2dc9
Sarah Li <sarcl@hotmail.com>
2018-06-22 15:21:42 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -29,5 +29,21 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 end
+class Book
+  attr_accessor :author, :page_count, :genre
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
 Harry_Potter = Book.new ("Harry potter and the sorcereor's stone")
 Harry_Potter.turn_page
